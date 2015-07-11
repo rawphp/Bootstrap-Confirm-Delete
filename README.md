a9880b82788e3f615b106c280348cd43a7ea0222
Tom Kaczocha <tom@crazydev.org>
2015-07-11 17:37:59 +1000

    Rebuilt plugin with a better template. Added usage examples to README.

--- README.md
+++ README.md
@@ -2,5 +2,99 @@
 
 Bootstrap confirm delete modal dialog jquery plugin
 
-## Examples
-Coming Soon
+
+## Installation
+You can install with [bower](http://bower.io/) or [npm](https://www.npmjs.com/).
+
+```sh
+$ bower install bootstrap-confirm-delete
+$ npm install --save bootstrap-confirm-delete
+```
+
+## Dependencies
+This plugin depends on the following:
+- jquery >= 1.9.1
+- bootstrap >= 3.3.5
+
+NOTE: This plugin has not been tested with versions prior to these.
+
+
+## Basic Usage
+This plugin will intercept the delete event on configured page elements. In this example it will block the deletion click
+on any links or buttons that have the 'delete' class on them.
+
+```html
+<script src="jquery.js"></script>
+<script src="bootstrap-confirm-delete.js"></script>
+```
+
+```html
+<a href="server.php" class="delete" data_type="post">Delete</a>
+```
+
+Initialise the plugin either in <script></script> tags on the html page or in an external .js script. 
+
+```js
+$( document ).ready( function( )
+{
+    $( '.delete' ).bootstrap_confirm_delete( );
+} );
+```
+
+Notice the 'data_type' attribute on the link. The plugin will use this attribute if set to show a custom delete message. For example,
+
+Heading:    'Delete Post'
+
+Message:    'Are you sure you want to delete this post?'
+
+
+## Options
+Bootstrap Confirm Delete plugin comes with several option settings.
+
+### Example
+This example shows all the available options:
+
+```js
+$( document ).ready( function( )
+{
+    $( '.delete' ).bootstrap_confirm_delete(
+        {
+            debug:              false,
+            heading:            'My Custom Delete Heading',
+            message:            'Are you sure you want to delete this item?',
+            data_type:          'post',
+            callback:           function() { console.log( 'deleting item over ajax' ); },
+            delete_callback:    function() { console.log( 'delete button clicked' ); },
+            cancel_callback:    function() { console.log( 'cancel button clicked' ); }
+        }
+    );
+} );
+```
+
+### debug
+Debug mode will output events and information to the console.
+
+### heading
+This is for setting a custom modal heading.
+
+### message
+Setting a custom delete message/question.
+
+### data_type
+Used if heading & message are not provided
+
+### callback
+Will fire if responding to a button click that has no href attribute.
+
+Use this callback to do any deletions from a button click.
+
+### delete_callback
+Will fire when the delete button is clicked and a handler is provided.
+
+### cancel_callback
+Will fire when the cancel button is clicked and a handler is provided.
+
+
+## License
+Copyright &copy; 2015 Tom Kaczocha <tom@rawphp.org>
+Licensed under the MIT license.
--- bootstrap-confirm-delete.js
+++ bootstrap-confirm-delete.js
@@ -1,29 +1,39 @@
 /**
- * Created by Tom on 9/07/2015.
+ * Bootstrap Confirm Delete
+ * Author: Tom Kaczocha <tom@rawphp.org>
+ * Licensed under the MIT license
  */
 
-( function ( $ )
+;
+( function ( $, window, document, undefined )
 {
-    $.fn.bootstrap_confirm_delete = function ( options )
+    var bootstrap_confirm_delete = function ( element, options )
     {
-        var settings = $.extend(
+        this.element = $( element );
+        this.settings = $.extend(
             {
+                debug: false,
                 heading: 'Delete',
                 message: 'Are you sure you want to delete this item?',
                 data_type: null,
-                callback: null
-            }, options );
+                callback: null,
+                delete_callback: null,
+                cancel_callback: null
+            }, options || {}
+        );
 
-        $( this ).on( 'click', function ( event )
+        this.onDelete = function ( event )
         {
             event.preventDefault();
+
+            var plugin = $( this ).data( 'bootstrap_confirm_delete' );
 
             if ( undefined !== $( this ).attr( 'data-type' ) )
             {
                 var name = $( this ).attr( 'data-type' );
 
-                settings.heading = 'Delete ' + name[ 0 ].toUpperCase() + name.substr( 1 );
-                settings.message = 'Are you sure you want to delete this ' + name + '?';
+                plugin.settings.heading = 'Delete ' + name[ 0 ].toUpperCase() + name.substr( 1 );
+                plugin.settings.message = 'Are you sure you want to delete this ' + name + '?';
             }
 
             if ( null === document.getElementById( 'bootstrap-confirm-delete-container' ) )
@@ -31,27 +41,59 @@
                 $( 'body' ).append( '<div id="bootstrap-confirm-delete-container"><div id="bootstrap-confirm-dialog" class="modal fade"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 id="bootstrap-confirm-dialog-heading"></h4></div><div class="modal-body"><p id="bootstrap-confirm-dialog-text"></p></div><div class="modal-footer"><button id="bootstrap-confirm-dialog-cancel-delete-btn" type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancel</button><a id="bootstrap-confirm-dialog-delete-btn" href="#" class="btn btn-danger pull-right">Delete</a></div></div></div></div></div>' );
             }
 
-            $( '#bootstrap-confirm-dialog-heading' ).html( settings.heading );
-            $( '#bootstrap-confirm-dialog-text' ).html( settings.message );
+            $( '#bootstrap-confirm-dialog-heading' ).html( plugin.settings.heading );
+            $( '#bootstrap-confirm-dialog-text' ).html( plugin.settings.message );
             $( '#bootstrap-confirm-dialog' ).modal( 'toggle' );
 
-            if ( null !== settings.callback )
+            if ( null !== plugin.settings.callback )
             {
                 $( '#bootstrap-confirm-dialog-delete-btn' ).attr( 'data-dismiss', 'modal' );
 
-                if ( $.isFunction( settings.callback ) )
+                if ( $.isFunction( plugin.settings.callback ) )
                 {
-                    $( 'a#bootstrap-confirm-dialog-delete-btn' ).on( 'click', { originalObject: this }, settings.callback );
+                    var deleteBtn = $( 'a#bootstrap-confirm-dialog-delete-btn' );
+
+                    deleteBtn.on( 'click', { originalObject: this }, plugin.settings.callback );
+
+                    if ( null !== plugin.settings.delete_callback )
+                    {
+                        deleteBtn.on( 'click', { originalObject: this }, plugin.settings.delete_callback );
+                    }
                 }
                 else
                 {
-                    console.log( settings.callback + ' is not a valid callback' );
+                    console.log( plugin.settings.callback + ' is not a valid callback' );
                 }
             }
             else if ( '' !== event.currentTarget.href )
             {
                 $( 'a#bootstrap-confirm-dialog-delete-btn' ).attr( 'href', event.currentTarget.href );
             }
+
+            if ( null !== plugin.settings.cancel_callback )
+            {
+                $( '#bootstrap-confirm-dialog-cancel-delete-btn' ).on( 'click', { originalObject: this }, plugin.settings.cancel_callback );
+            }
+        };
+    };
+
+    $.fn.bootstrap_confirm_delete = function ( options )
+    {
+        return this.each( function ()
+        {
+            var element = $( this );
+
+            if ( element.data( 'bootstrap_confirm_delete' ) )
+            {
+                return element.data( 'bootstrap_confirm_delete' );
+            }
+
+            var plugin = new bootstrap_confirm_delete( this, options );
+
+            element.data( 'bootstrap_confirm_delete', plugin );
+            element.on( 'click', plugin.onDelete );
+
+            return plugin;
         } );
     };
-}( jQuery ));
+}( jQuery, window, document, undefined ));
