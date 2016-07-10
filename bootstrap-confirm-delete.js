f1df99299b431c88b77581483e034377e77e10d5
Tom Kaczocha <tom@rawphp.org>
2016-07-10 12:21:45 +1000

    Fix variable styles

--- bootstrap-confirm-delete.js
+++ bootstrap-confirm-delete.js
@@ -15,8 +15,8 @@
                 debug: false,
                 heading: 'Delete',
                 message: 'Are you sure you want to delete this item?',
-                btnOkLabel: 'Yes',
-                btnCancelLabel: 'Cancel',
+                btn_ok_label: 'Yes',
+                btn_cancel_label: 'Cancel',
                 data_type: null,
                 callback: null,
                 delete_callback: null,
@@ -45,8 +45,8 @@
 
             $( '#bootstrap-confirm-dialog-heading' ).html( plugin.settings.heading );
             $( '#bootstrap-confirm-dialog-text' ).html( plugin.settings.message );
-            $( '#bootstrap-confirm-dialog-delete-btn' ).html( plugin.settings.btnOkLabel );
-            $( '#bootstrap-confirm-dialog-cancel-delete-btn' ).html( plugin.settings.btnCancelLabel );
+            $( '#bootstrap-confirm-dialog-delete-btn' ).html( plugin.settings.btn_ok_label );
+            $( '#bootstrap-confirm-dialog-cancel-delete-btn' ).html( plugin.settings.btn_cancel_label );
             $( '#bootstrap-confirm-dialog' ).modal( 'toggle' );
 
             var deleteBtn = $( 'a#bootstrap-confirm-dialog-delete-btn' );
